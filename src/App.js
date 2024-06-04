e335d5082c19d7654f8f9b8b560dd98c19154353
Vivin Anand <vivinanand@MacBook-Kaathu-M1-2021.local>
2024-06-04 16:22:56 +0530

    Vivin's Signin branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home';
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import SignIn from './Components/SignIn';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+          <div>
+            <Routes>
+              <Route exact path='/' element={<SignIn />}/>
+              <Route exact path='/login' element={<Home />}/>
+            </Routes>
+          </div>
+        </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/SignIn.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+
+function SignIn() {
+
+  const handleSuccess = (credentialResponse) => {
+    console.log(credentialResponse);
+  };
+
+  const handleError = () => {
+    console.log('Login Failed');
+  };
+
+  return (
+    <div>
+        <p>Sign in with Google below!</p>
+        <br />
+        <GoogleOAuthProvider clientId="597831817687-ekv1o2ukvp4mr8lhjpv60deeq2ul2k3o.apps.googleusercontent.com">
+            <GoogleLogin
+            onSuccess={handleSuccess}
+            onError={handleError}
+            />
+        </GoogleOAuthProvider>
+    </div>
+  );
+}
+
+export default SignIn
